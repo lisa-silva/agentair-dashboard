6e9e88b6f729d572621f48d2c74eb9ffb0e056b9
Lisa Silva <cuddlepolotics@gmail.com>
2026-03-01 15:49:56 -0800

    Create auth.py

---
+++ utils/auth.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import hmac
+
+def check_password():
+    """Simple password check."""
+    def password_entered():
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if st.session_state.get("password_correct", False):
+        return True
+
+    st.text_input("Password", type="password", on_change=password_entered, key="password")
+    return False
